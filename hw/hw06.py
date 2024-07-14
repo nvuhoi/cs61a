e4bc9ef88b2b33727fe3e84ebe73481bcd48fe3b
黃振恩 <144233626+nvuhoi@users.noreply.github.com>
2024-07-15 02:43:25 +0800

    Update hw06.py

--- hw/hw06.py
+++ hw/hw06.py
@@ -105,30 +105,6 @@ def store_digits(n):
     return store_last(n)
 
 
-
-
-def deep_map_mut(func, lnk):
-    """Mutates a deep link lnk by replacing each item found with the
-    result of calling func on the item. Does NOT create new Links (so
-    no use of Link's constructor).
-
-    Does not return the modified Link object.
-
-    >>> link1 = Link(3, Link(Link(4), Link(5, Link(6))))
-    >>> print(link1)
-    <3 <4> 5 6>
-    >>> # Disallow the use of making new Links before calling deep_map_mut
-    >>> Link.__init__, hold = lambda *args: print("Do not create any new Links."), Link.__init__
-    >>> try:
-    ...     deep_map_mut(lambda x: x * x, link1)
-    ... finally:
-    ...     Link.__init__ = hold
-    >>> print(link1)
-    <9 <16> 25 36>
-    """
-    "*** YOUR CODE HERE ***"
-
-
 def two_list(vals, counts):
     """
     Returns a linked list according to the two lists that were passed in. Assume
