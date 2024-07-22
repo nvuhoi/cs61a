3eca73e4df0a63d2aa3902a222067cd1fda743f2
黃振恩 <144233626+nvuhoi@users.noreply.github.com>
2024-07-23 02:43:16 +0800

    Add files via upload

---
+++ hw/hw09.scm
@@ -0,0 +1,19 @@
+(define (curry-cook formals body) 
+      (if (= (length formals) 1)
+        `(lambda ,formals ,body)
+        `(lambda (,(car formals)) ,(curry-cook (cdr formals) body))))
+      
+
+(define (curry-consume curry args)
+  (if (= (length args) 1)
+    (curry (car args))
+    (curry-consume (curry (car args)) (cdr args))))
+
+(define-macro (switch expr options)
+  (switch-to-cond (list 'switch expr options)))
+
+(define (switch-to-cond switch-expr)
+  (cons 'cond
+        (map (lambda (option)
+               (cons (list 'equal? (cadr switch-expr) (car option)) (cdr option)))
+             (car (cdr (cdr switch-expr))))))
